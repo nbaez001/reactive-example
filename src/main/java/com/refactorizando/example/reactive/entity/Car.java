1e6afceb9dff7a4e1ea2753bcc6c365d1e436dfb
Your Name <you@example.com>
2022-12-13 15:23:09 -0500

    first commit

---
+++ src/main/java/com/refactorizando/example/reactive/entity/Car.java
@@ -0,0 +1,28 @@
+package com.refactorizando.example.reactive.entity;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Car implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Long id;
+
+	private String model;
+
+	private String brand;
+
+	private String color;
+
+}
---
+++ src/test/java/CarServiceTest.java
@@ -0,0 +1,36 @@
+import java.time.Duration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+
+import com.refactorizando.example.reactive.entity.Car;
+import com.refactorizando.example.reactive.service.CarService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@SpringBootTest
+public class CarServiceTest {
+
+	@Autowired
+	CarService carService;
+
+	@Test
+	void testFindById() {
+		Mono<Car> uno = carService.findById(1L);
+		StepVerifier.create(uno).expectNextMatches(obj -> obj.getModel().equals("MUSTANG")).verifyComplete();
+	}
+
+	@Test
+	void testFindAll() {
+		Flux<Car> uno = carService.findAll(Sort.by("model", "brand"));
+		StepVerifier.create(uno)
+				.expectNextMatches(obj -> obj.getModel().equals("MUSTANG"))
+				.thenAwait(Duration.ofSeconds(2))
+				.expectNextMatches(obj -> obj.getModel().equals("TOLEDO"))
+				.verifyComplete();
+	}
+}
